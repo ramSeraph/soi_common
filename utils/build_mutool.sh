27b1cb9f27719869f85240b9e1f3dd602330a0ae
ramSeraph <kandimalla.sreeram@gmail.com>
2025-10-19 18:56:09 +0530

    assume patch is in the same dir

--- utils/build_mutool.sh
+++ utils/build_mutool.sh
@@ -5,7 +5,9 @@ git clone https://github.com/ArtifexSoftware/mupdf
 cd mupdf
 git checkout 1.20.0-rc1
 git submodule update --init
-git apply ../mupdf.patch
+# pickup patch from same folder as the script
+script_dir=$(dirname "$0")
+git apply ${script_dir}/mupdf.patch
 make HAVE_X11=no HAVE_GLUT=no
 mkdir -p ../bin
 cp build/release/mutool ../bin/ 
